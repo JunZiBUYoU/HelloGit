637fe8c4041af21b8eb6ffeef25c5bd89a5ebd1d
JuZiBUYoU <1364046598@qq.com>
2021-01-08 20:55:46 +0800

    添加文件

---
+++ GitHello/src/main/java/com/zhaoyan/git/HelloGit.java
@@ -0,0 +1,9 @@
+package com.zhaoyan.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
